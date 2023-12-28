4fde5be8c30466a5611697d3b50b8a80b8a048bf
MasterPC <free_person@inbox.ru>
2023-12-28 14:00:24 +0300

    Загрузка данных о пользователе

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,12 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.service.*;
+
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.stream.Collectors;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -23,6 +24,7 @@ public class IndexController {
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -41,7 +43,19 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
+        var interviews = interviewsService.getByType(1);
+        model.addAttribute("new_interviews", interviews);
+        var submitters = interviews.stream()
+                .map(InterviewDTO::getSubmitterId)
+                .map(profilesService::getProfileById)
+                .map(profile -> {
+                    if (profile.isPresent()) {
+                        return profile.get().getUsername();
+                    }
+                    return "username";
+                })
+                .collect(Collectors.toList());
+        model.addAttribute("submitters", submitters);
         return "index";
     }
 }
