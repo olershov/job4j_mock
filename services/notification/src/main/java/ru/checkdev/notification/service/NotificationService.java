013e6bce59034bb64b70e87cde160bed52decbfe
MasterPC <free_person@inbox.ru>
2024-03-07 00:05:28 +0300

    added Kafka in NotificationService

--- services/notification/src/main/java/ru/checkdev/notification/service/NotificationService.java
+++ services/notification/src/main/java/ru/checkdev/notification/service/NotificationService.java
@@ -1,6 +1,8 @@
 package ru.checkdev.notification.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.checkdev.notification.domain.Notify;
 
@@ -9,22 +11,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 @Service
+@AllArgsConstructor
 public class NotificationService {
 
-    private final TemplateService templates;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-    @Autowired
-    public NotificationService(final TemplateService templates) {
-        this.templates = templates;
-    }
+    private final KafkaTemplate<String, Notify> kafkaTemplate;
 
     public void put(final Notify notify) {
-        this.scheduler.execute(() -> this.templates.send(notify));
-    }
-
-    @PreDestroy
-    public void close() {
-        this.scheduler.shutdown();
+        kafkaTemplate.send("notify", notify);
     }
 }
